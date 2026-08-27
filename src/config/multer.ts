ad0688a3cbddeabf1445b1b0832b4e14c73f8088
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-08-27 15:08:56 +0300

    config update

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,24 +1,45 @@
-import multer from 'multer'
+import multer from 'multer';
+import path from 'path';
 
 const allowedMimeTypes = [
-    "image/png",
-    "image/jpeg",
-    "image/jpg",
-    "image/webp"
-]
+  'image/png',
+  'image/jpeg',
+  'image/webp',
+];
+
+const allowedExtensions = [
+  '.png',
+  '.jpg',
+  '.jpeg',
+  '.webp',
+];
 
 export const upload = multer({
-    storage: multer.memoryStorage(),
-    
-    limits: {
-        fileSize: 5 * 1024 * 1024,
-    },
+  storage: multer.memoryStorage(),
 
-    fileFilter(req,file,cb) {
-        if(!allowedMimeTypes.includes(file.mimetype)){
-            return cb(new Error('Only images are allowed'))
-        }
-        cb(null,true)
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+
+  fileFilter: (_req, file, cb) => {
+    console.log('FILE:', {
+      originalname: file.originalname,
+      mimetype: file.mimetype,
+    });
+
+    const extension = file.originalname
+      ? path.extname(file.originalname).toLowerCase()
+      : '';
+
+    if (
+      !allowedMimeTypes.includes(file.mimetype) &&
+      !allowedExtensions.includes(extension)
+    ) {
+      return cb(
+        new Error('Only PNG, JPEG and WEBP images are allowed'),
+      );
     }
-})
 
+    cb(null, true);
+  },
+});
