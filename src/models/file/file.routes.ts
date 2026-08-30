7809662dc71d02148a2ef8f200034085a6f5dfc1
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-08-30 19:24:28 +0300

    file.routes.ts update

--- src/models/file/file.routes.ts
+++ src/models/file/file.routes.ts
@@ -6,9 +6,10 @@ import { fileIdSchema } from "./file.validation";
 
 const router = Router();
 
-router.post("/upload",uploadImage,fileController.upload);
-router.get("/",fileController.getAll);
-router.get("/:id",validate(fileIdSchema),fileController.getById);
-router.delete("/:id",validate(fileIdSchema),fileController.delete);
+
+router.post("/upload", uploadImage, fileController.upload);
+router.get("/", fileController.getAll);
+router.get("/:id", validate(fileIdSchema), fileController.getById);
+router.delete("/:id", validate(fileIdSchema), fileController.delete);
 
 export default router;
