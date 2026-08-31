e4ade4e983f706d6507a8c9206a9e15f9b8f2de0
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-08-31 16:00:17 +0300

    file.routes update

--- src/models/file/file.routes.ts
+++ src/models/file/file.routes.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import fileController from "./file.controller";
 import { uploadImage } from "@/middlewares/upload.middleware";
 import { validate } from "@/middlewares/validation.middleware";
+import { uploadLimiter } from "@/middlewares/rateLimit.middleware";
 import { fileIdSchema } from "./file.validation";
 
 const router = Router();
 
 
-router.post("/upload", uploadImage, fileController.upload);
+router.post("/upload", uploadLimiter, uploadImage, fileController.upload);
 router.get("/", fileController.getAll);
 router.get("/:id", validate(fileIdSchema), fileController.getById);
 router.delete("/:id", validate(fileIdSchema), fileController.delete);
