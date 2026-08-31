9c112ff0556a078c77f67de4040b3ab2b3d53383
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-08-31 11:37:20 +0300

    rate-limit add

---
+++ src/middlewares/rateLimit.middleware.ts
@@ -0,0 +1,23 @@
+import rateLimit from "express-rate-limit";
+
+export const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many requests, please try again later.",
+  },
+});
+
+export const uploadLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many upload requests, please try again later.",
+  },
+});
