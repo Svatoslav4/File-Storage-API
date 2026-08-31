e17b9e14d9aa125a6d85194004d0081a6f2e01b6
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-08-31 16:01:53 +0300

    app.ts update

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
-import express from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
-import fileRoutes from '@/models/file/file.routes'
-import { errorMiddleware } from '@/middlewares/error.middleware'
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
+import fileRoutes from '@/models/file/file.routes';
+import { errorMiddleware } from '@/middlewares/error.middleware';
+import { apiLimiter } from '@/middlewares/rateLimit.middleware';
+import { swaggerSpec } from '@/docs/swagger';
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(apiLimiter);
 
 app.get('/', (_req, res) => {
   res.status(200).json({
@@ -17,7 +21,8 @@ app.get('/', (_req, res) => {
   });
 });
 
-app.use('/files', fileRoutes);
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec),);
+app.use('/api/files', fileRoutes);
 
 app.use(errorMiddleware);
 
